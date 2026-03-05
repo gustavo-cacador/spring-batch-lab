242f1e310ecb4caa198bba7494d02718d7a04215
Gustavo <gustavoirl456@outlook.com>
2026-03-04 22:23:16 -0300

    Projeto criado, Job e Step configurados

---
+++ src/main/java/com/gustavoronchi/hello_world_spring_batch/config/BatchConfig.java
@@ -0,0 +1,35 @@
+package com.gustavoronchi.hello_world_spring_batch.config;
+
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.StepContribution;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.infrastructure.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class BatchConfig {
+
+    @Bean
+    public Job job(JobRepository jobRepository, Step step) {
+        return new JobBuilder("job", jobRepository)
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step", jobRepository)
+                // estou utilizando tasklet pois é uma tarefa simples (apenas imprimir "Olá Mundo")
+                .tasklet((StepContribution contribution, ChunkContext chunkContext) -> {
+                    System.out.println("Olá Mundo");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}
