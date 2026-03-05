35a2671f4f60f2794df7f2b3be44e71e8fa915c4
Gustavo <gustavoirl456@outlook.com>
2026-03-04 22:48:38 -0300

    refatorando step e tasklet

---
+++ src/main/java/com/gustavoronchi/hello_world_spring_batch/tasklet/PrintHelloTasklet.java
@@ -0,0 +1,20 @@
+package com.gustavoronchi.hello_world_spring_batch.tasklet;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.StepContribution;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.infrastructure.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+@StepScope
+public class PrintHelloTasklet implements Tasklet {
+
+    @Override
+    public @Nullable RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("Olá Mundo");
+        return RepeatStatus.FINISHED;
+    }
+}
