813247793da330e12478b0ce6708247def83de7f
Gustavo Caçador <gustavoirl456@outlook.com>
2026-03-09 21:20:16 -0300

    injetando valor com environment variables

--- src/main/java/com/gustavoronchi/hello_world_spring_batch/tasklet/PrintHelloTasklet.java
+++ src/main/java/com/gustavoronchi/hello_world_spring_batch/tasklet/PrintHelloTasklet.java
@@ -6,15 +6,19 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.StepContribution;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.infrastructure.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @StepScope
 public class PrintHelloTasklet implements Tasklet {
 
+    @Value("${name}")
+    private String name;
+
     @Override
     public @Nullable RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        System.out.println("Olá Mundo");
+        System.out.println("Olá " + name + "!");
         return RepeatStatus.FINISHED;
     }
 }
